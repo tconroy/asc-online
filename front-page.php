50de71dd3f95f3c78d10caabdd0fcccacced329d
Tom Conroy <tdc5536@rit.edu>
2014-09-09 10:30:56 -0400

    more work, mainly on the footer

--- front-page.php
+++ front-page.php
@@ -1,4 +1,6 @@
-<?php// get_template_part('templates/page', 'header'); ?>
+<?php
+ // get_template_part('templates/page', 'header');
+ ?>
 
 <!-- did have a class of "container" -->
 <div class="">
@@ -17,9 +19,9 @@
           </div>
           <div class="col-xs-6">
             <div class="iphone-wrapper visible-xs">
-              <img class="iphone" src="<?=get_template_directory_uri()?>/assets/img/content/home/iphone.png" alt="">
+              <img class="iphone" src="<?php echo get_template_directory_uri() ?>/assets/img/content/home/iphone.png" alt="">
               <div class="content">
-                <img class="logo" src="<?=get_template_directory_uri();?>/assets/img/layout/logo/ascoLogoMobile-noshadow.png" alt="">
+                <img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/layout/logo/ascoLogoMobile-noshadow.png" alt="">
                 <p>ASC Online</p>
               </div>
             </div>
@@ -50,38 +52,40 @@
   </div>
 
   <div class="home whatsnew hidden-xs">
-    <!--
-    <div class="col-sm-12">
+    <div class="container">
+
       <div class="title">
         <h2>What's New</h2>
         <small>ASC Sneak Peak</small>
       </div>
+
       <div class="macbook-wrapper">
-        <iframe width="715" height="402" src="//www.youtube.com/embed/cq3q5cKU3NU?rel=0" frameborder="0" allowfullscreen></iframe>
+        <div class="inner-macbook">
+          <iframe src="//www.youtube.com/embed/DAYDZBD3pT8?rel=0"
+                  width="560"
+                  height="315"
+                  frameborder="0"
+                  allowfullscreen class="fluid-width-video-wrapper">
+          </iframe>
+        </div>
       </div>
-    </div>-->
-<div class="col-sm-12">
-  <div class="title">
-    <h2>What's New</h2>
-    <small>ASC Sneak Peak</small>
+
+    </div>
   </div>
-  <div class="macbook-wrapper">
-      <div class="inner-macbook">
-    <iframe src="//www.youtube.com/embed/cq3q5cKU3NU?rel=0" width="560" height="315" frameborder="0" allowfullscreen class="fluid-width-video-wrapper"></iframe>
-  </div>
-  </div>
-</div>
-  </div>
+
 
   <div class="home classroom container">
         <div class="title col-sm-12">
+        <div class="title-cont">
           <h2>ASC Online Classroom</h2>
           <small>Lessons worth Learning</small>
+        </div>
         </div>
         <div class="col-sm-6 col-md-4">
 
           <div class="panel panel-default blue">
-            <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri();?>/assets/img/content/lessons/study_skills_icon.png" alt=""></a>
+            <a href=""><img class="img-responsive" src="<?php
+echo get_template_directory_uri(); ?>/assets/img/content/lessons/study_skills_icon.png" alt=""></a>
             <div class="panel-body">
               <h3 class="panel-title"><a href="">Study Skills</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque voluptatibus tempore blanditiis.</p>
@@ -91,7 +95,8 @@
 
         <div class="col-sm-6 col-md-4">
           <div class="panel panel-default green">
-            <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri();?>/assets/img/content/lessons/time_management_icon.png" alt=""></a>
+            <a href=""><img class="img-responsive" src="<?php
+echo get_template_directory_uri(); ?>/assets/img/content/lessons/time_management_icon.png" alt=""></a>
             <div class="panel-body">
               <h3 class="panel-title"><a href="">Time Management</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque voluptatibus tempore blanditiis.</p>
@@ -103,7 +108,8 @@
 
         <div class="col-sm-6 col-md-4">
           <div class="panel panel-default orange">
-            <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri();?>/assets/img/content/lessons/tutoring_icon.png" alt=""></a>
+            <a href=""><img class="img-responsive" src="<?php
+echo get_template_directory_uri(); ?>/assets/img/content/lessons/tutoring_icon.png" alt=""></a>
             <div class="panel-body">
               <h3 class="panel-title"><a href="">Tutoring</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque voluptatibus tempore blanditiis.</p>
@@ -111,11 +117,12 @@
           </div>
         </div>
 
-         <div class="clearfix visible-md visible-lg"></div>
+         <div class="clearfix visible-md visible-lg lcf"></div>
 
         <div class="col-sm-6 col-md-4">
           <div class="panel panel-default yellow">
-            <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri();?>/assets/img/content/lessons/math_icon.png" alt=""></a>
+            <a href=""><img class="img-responsive" src="<?php
+echo get_template_directory_uri(); ?>/assets/img/content/lessons/math_icon.png" alt=""></a>
             <div class="panel-body">
               <h3 class="panel-title"><a href="">Math</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque voluptatibus tempore blanditiis.</p>
@@ -127,7 +134,8 @@
 
         <div class="col-sm-6 col-md-4">
           <div class="panel panel-default red">
-            <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri();?>/assets/img/content/lessons/reading_icon.png" alt=""></a>
+            <a href=""><img class="img-responsive" src="<?php
+echo get_template_directory_uri(); ?>/assets/img/content/lessons/reading_icon.png" alt=""></a>
             <div class="panel-body">
               <h3 class="panel-title"><a href="">Reading</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque voluptatibus tempore blanditiis.</p>
@@ -137,7 +145,8 @@
 
         <div class="col-sm-6 col-md-4">
           <div class="panel panel-default gray">
-            <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri();?>/assets/img/content/lessons/online_learning_icon.png" alt=""></a>
+            <a href=""><img class="img-responsive" src="<?php
+echo get_template_directory_uri(); ?>/assets/img/content/lessons/online_learning_icon.png" alt=""></a>
             <div class="panel-body">
               <h3 class="panel-title"><a href="">Online Learning</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque voluptatibus tempore blanditiis.</p>
--- templates/asco-footer.php
+++ templates/asco-footer.php
@@ -6,18 +6,42 @@
 </footer> -->
 
 <footer class="footer content-info" role="contentinfo">
-  <div class="learnmore-footer">
+  <div class="learnmore-footer container-fluid">
     <h2>Learn More</h2>
-      <a class="browser"  href="#"><span>ASC Website</span></a>
-      <a class="schedule" href="#"><span>ASC Schedules</span></a>
-      <a class="mail"     href="#"><span>Contact the ASC</span></a>
+      <ul class="list-inline col-xs-12">
+        <li class="col-xs-12 col-sm-4"><a class="browser"  href="#">ASC Website</a></li>
+        <li class="col-xs-12 col-sm-4"><a class="schedule" href="#">ASC Schedules</a></li>
+        <li class="col-xs-12 col-sm-4"><a class="mail"     href="#">Contact the ASC</a></li>
+      </ul>
   </div>
   <div class="stayconnected-footer">
     <h2>Visit <a href="#">ASC</a> and Stay Connected</h2>
+
+    <ul class="list-inline social-circle">
+      <li><a class="fb" href="" title="Facebook"></a></li>
+      <li><a class="ig" href="" title="Instagram"></a></li>
+      <li><a class="tw" href="" title="Twitter"></a></li>
+      <li><a class="yt" href="" title="Youtube"></a></li>
+    </ul>
+
+    <ul class="list-inline">
+      <li><a href="www.rit.edu">Copyright 2013 Rochester Institute of Technology</a></li>
+      <li><a href="www.rit.edu/studentaffairs/">Student Affairs</a></li>
+      <li><a href="www.rit.edu/studentaffairs/asc">Academic Support Center</a></li>
+      <li>All Rights Reserved.</li>
+    </ul>
   </div>
   <div class="rit-footer">
-      <small><a href="http://www.rit.edu"><img src="<?=get_template_directory_uri();?>/assets/img/layout/logo/ritLogo.png" alt=""></a></small>
-      <small>1 Lomb Memorial Drive, Rochester, NY 14623</small>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <a href="http://www.rit.edu">
+          <img src="<?=get_template_directory_uri();?>/assets/img/layout/logo/ritLogo.png">
+        </a>
+      </div>
+      <div class="col-xs-12 col-sm-6 text-right">
+        <small>1 Lomb Memorial Drive, Rochester, NY 14623</small>
+      </div>
+    </div>
   </div>
 </footer>
 
