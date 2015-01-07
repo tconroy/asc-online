5769ca291f7d70018f06cb706d4ed8df5ac3e458
Tom Conroy <tdc5536@rit.edu>
2015-01-06 19:05:23 -0500

    minor css modifications, uploaded to staging

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4b7dfebc9675e67ce845f107b34720f3');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '99ce7457ebb32282ab94423b4e97c744');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
